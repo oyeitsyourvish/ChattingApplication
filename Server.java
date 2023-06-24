7806f2e2aeaf1f88c7892d7f555e964230b9c504
Vishal <chinchanevishal03@gmail.com>
2023-06-24 11:08:35 +0530

    Added profile icons

--- Server.java
+++ Server.java
@@ -1,16 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-class Server extends JFrame {
+class Server extends JFrame implements ActionListener {
 
     Server() {
+
+        // This is for Green Line box
         setLayout(null);
         JPanel p1 = new JPanel();
         p1.setBackground(new Color(7, 94, 84));
-        p1.setBounds(0, 0, 450, 60);
+        p1.setBounds(0, 0, 450, 70);
         p1.setLayout(null);
         add(p1);
 
+        // This is for Back Button
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
         Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
@@ -18,10 +22,46 @@ class Server extends JFrame {
         back.setBounds(5, 20, 25, 25);
         p1.add(back);
 
+        back.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent ae) {
+                System.exit(0);
+            }
+
+        });
+
+        // This is for Profile Button
+        ImageIcon i4 = new ImageIcon(ClassLoader.getSystemResource("icons/1.png"));
+        Image i5 = i4.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+        ImageIcon i6 = new ImageIcon(i5);
+        JLabel profile = new JLabel(i6);
+        profile.setBounds(40, 10, 50, 50);
+        p1.add(profile);
+
+        // This is for Video call option Button
+        ImageIcon i7 = new ImageIcon(ClassLoader.getSystemResource("icons/video.png"));
+        Image i8 = i7.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        ImageIcon i9 = new ImageIcon(i8);
+        JLabel video = new JLabel(i9);
+        video.setBounds(300, 20, 30, 30);
+        p1.add(video);
+
+        // This is for Voice call option Button
+        ImageIcon i10 = new ImageIcon(ClassLoader.getSystemResource("icons/phone.png"));
+        Image i11 = i10.getImage().getScaledInstance(30, 30, Image.SCALE_DEFAULT);
+        ImageIcon i12 = new ImageIcon(i11);
+        JLabel voice = new JLabel(i12);
+        voice.setBounds(350, 20, 30, 30);
+        p1.add(voice);
+
+        // This is for all screen layout height width
         setSize(450, 700);
         getContentPane().setBackground(Color.WHITE);
 
         setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+
     }
 
     public static void main(String[] args) {
